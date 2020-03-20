aabade06c11cd46439767eff2e8c22a0cbb64485
Lam Nguyen <lamn@cec.sc.edu>
2020-03-20 16:00:26 -0400

    2020-03-20 1600 update to views

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import get_object_or_404, get_list_or_404, render
+from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
@@ -25,8 +27,27 @@ def detail(request, question_id):
     return render(request, 'main/detail.html', {'question': question})
 
 def results(request, question_id):
-    choices = get_list_or_404(Choice, pk=question_id)
-    return render(request, 'main/results.html', {'choices': choices})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+#part 4
+class IndexView(generic.ListView):
+    template_name = 'main/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'main/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'main/results.html'
--- neXtInstitute/wsgi.py
+++ neXtInstitute/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stockDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neXtInstitute.settings')
 
 application = get_wsgi_application()
